cdc523db4b99a51b6150a294c8a69a2a84583c74
YounHooooo <yh6316l@naver.com>
2024-04-08 17:15:33 +0900

    Calculator

--- my-app/src/components/Calculator/index.jsx
+++ my-app/src/components/Calculator/index.jsx
@@ -1,14 +1,64 @@
-import { useState } from "react";
-import styles from "./index.css";
+import React, { useState } from "react";
 
 export default function Home() {
-    const [number, setNumber] = useState(0);
-
+    const [number, setNumber] = useState('');
     const [holdNumber, setHoldNumber] = useState(0);
-    const [operation, setOperration] = useState('');
+    const [operation, setOperation] = useState('');
 
     const handleChangeNumber = (enteredNumber) => {
-        setNumber(enteredNumber)
+        setNumber(parseFloat(String(number) + enteredNumber))
+    }
+
+    const handleRemoveNumber = () => {
+        const numberString = String(number);
+
+        if(numberString.length === 0) {
+            setNumber(0)
+
+        } else {
+            setNumber(parseFloat(numberString.slice(0, numberString.length - 1)))
+        }
+    }
+
+    const handleClickOperation = (enteredOperation) => {
+        if (number != 0) {
+            setOperation(enteredOperation)
+
+            setHoldNumber(number)
+
+            setNumber(0)
+        }
+
+    }
+
+
+    const handleCalculation = () => {
+        switch (operation) {
+
+            case '+':
+                setNumber(holdNumber + number)
+                break;
+
+            case '-':
+                setNumber(holdNumber - number)
+                break;
+
+            case '*':
+                setNumber(holdNumber * number)
+                break;
+
+            case '/':
+                setNumber(holdNumber / number)
+                break;
+
+            case '%':
+                setNumber(holdNumber % number)
+                break;
+
+            default:
+                setNumber(holdNumber + number)
+                break;
+        }
     }
 
     const Head = () => {
@@ -16,6 +66,41 @@ export default function Home() {
             <div></div>
         );
     }
+
+
+    // css
+    const styles = {
+        container: {
+            border: "1px solid black",
+            padding: "20px",
+            width: "220px",
+            // minWidth: "14vw",
+            margin: "0 auto",
+        },
+        
+        calculated: {
+            border: "1px solid black",
+            padding: "10px 10px 10px 10px",
+            // width: "12vw",
+            width: "193px",
+        },
+
+        btn: {
+            width: "50px",
+            height: "50px",
+            margin: "5px 5px 5px 0",
+        },
+        
+        main: {
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-between",
+            aligIitems: "center",
+            paddingTop: "1rem",
+            minHeight: "100vh",
+        },
+    };
+
 
     return (
         <>
@@ -25,34 +110,40 @@ export default function Home() {
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <link rel="icon" href="%PUBLIC_URL%/favicon.ico" />
             </Head>
-            <main className={styles.main}>
-                <div className={styles.container}>
-                    <div className={styles.calculated}>
-                        {0}
+            <main style={styles.main} className={styles.main}>
+                <div style={styles.container} className={styles.container}>
+                    <div style={styles.calculated} className={styles.calculated}>
+                        {number}
                     </div>
                     <div>
-                        <button className={styles.btn}>.</button>
-                        <button className={styles.btn}>%</button>
-                        <button className={styles.btn}>AC</button>
-                        <button className={styles.btn}>{'<-'}</button>
+                        <button style={styles.btn} className={styles.btn}>.</button>
+                        <button style={styles.btn} className={styles.btn} onClick={() => handleClickOperation('%')}>%</button>
+                        <button style={styles.btn} className={styles.btn} onClick={() => setNumber(0)}>AC</button>
+                        <button style={styles.btn} className={styles.btn} onClick={handleRemoveNumber}>{'<-'}</button>
                     </div>
                     <div>
-                        <button className={styles.btn} onClick={() => handleChangeNumber(1)}>1</button>
-                        <button className={styles.btn} onClick={() => handleChangeNumber(2)}>2</button>
-                        <button className={styles.btn} onClick={() => handleChangeNumber(3)}>3</button>
-                        <button className={styles.btn} >+</button>
+                        <button style={styles.btn} className={styles.btn} onClick={() => handleChangeNumber(1)}>1</button>
+                        <button style={styles.btn} className={styles.btn} onClick={() => handleChangeNumber(2)}>2</button>
+                        <button style={styles.btn} className={styles.btn} onClick={() => handleChangeNumber(3)}>3</button>
+                        <button style={styles.btn} className={styles.btn} onClick={() => handleClickOperation('+')}>+</button>
                     </div>
                     <div>
-                        <button className={styles.btn} onClick={() => handleChangeNumber(4)}>4</button>
-                        <button className={styles.btn} onClick={() => handleChangeNumber(5)}>5</button>
-                        <button className={styles.btn} onClick={() => handleChangeNumber(6)}>6</button>
-                        <button className={styles.btn} >-</button>
+                        <button style={styles.btn} className={styles.btn} onClick={() => handleChangeNumber(4)}>4</button>
+                        <button style={styles.btn} className={styles.btn} onClick={() => handleChangeNumber(5)}>5</button>
+                        <button style={styles.btn} className={styles.btn} onClick={() => handleChangeNumber(6)}>6</button>
+                        <button style={styles.btn} className={styles.btn} onClick={() => handleClickOperation('-')}>-</button>
                     </div>
                     <div>
-                        <button className={styles.btn} onClick={() => handleChangeNumber(7)}>7</button>
-                        <button className={styles.btn} onClick={() => handleChangeNumber(8)}>8</button>
-                        <button className={styles.btn} onClick={() => handleChangeNumber(9)}>9</button>
-                        <button className={styles.btn}>*</button>
+                        <button style={styles.btn} className={styles.btn} onClick={() => handleChangeNumber(7)}>7</button>
+                        <button style={styles.btn} className={styles.btn} onClick={() => handleChangeNumber(8)}>8</button>
+                        <button style={styles.btn} className={styles.btn} onClick={() => handleChangeNumber(9)}>9</button>
+                        <button style={styles.btn} className={styles.btn} onClick={() => handleClickOperation('*')}>*</button>
+                    </div>
+                    <div>
+                        <button style={styles.btn} className={styles.btn} onClick={() => handleChangeNumber(0)}>0</button>
+                        <button style={styles.btn} className={styles.btn} onClick={() => handleClickOperation('-')}>-</button>
+                        <button style={styles.btn} className={styles.btn} onClick={handleCalculation}>=</button>
+                        <button style={styles.btn} className={styles.btn} onClick={() => handleClickOperation('/')}>/</button>
                     </div>
                 </div>
             </main>
