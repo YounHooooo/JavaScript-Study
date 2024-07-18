d88627faacc574da51cf2358d30d3ef45f68a8e1
YounHooooo <yh6316l@naver.com>
2024-07-18 22:50:35 +0900

    JS study

--- JS_Note/index.js
+++ JS_Note/index.js
@@ -61,16 +61,99 @@
 
 // body.appendChild(title);
 
-const button = document.querySelector("button");
-const removeButton = document.querySelector(".remove");
+// const button = document.querySelector("button");
+// const removeButton = document.querySelector(".remove");
 
-function handler(event) {
-  console.log("★");
-}
+// function handler(event) {
+//   console.log("★");
+// }
 
-function removeHandler(event) {
-  button.removeEventListener("click", handler);
-}
+// function removeHandler(event) {
+//   button.removeEventListener("click", handler);
+// }
 
-button.addEventListener("click", handler);
-removeButton.addEventListener("click", removeHandler);
+// button.addEventListener("click", handler);
+// removeButton.addEventListener("click", removeHandler);
+
+// // if문
+// const a = 10;
+// const b = 20;
+
+// if (a < b) {
+//   console.log("a가 더 작아요");
+// }
+
+// // switch문
+
+// const number = 10;
+
+// switch (number) {
+//   case 1:
+//     console.log(number);
+//     break;
+//   case 2:
+//     console.log(number);
+//     break;
+//   default:
+//     console.log("아무것도 해당되지 않아요");
+// }
+
+// 모듈러 홀짝
+
+// const number = 10;
+
+// if (number % 2 === 0) {
+//   console.log("짝수");
+// } else {
+//   console.log("홀수");
+// }
+
+// switch (number % 2) {
+//   case 0:
+//     console.log("짝수");
+//     break;
+//   case 1:
+//     console.log("홀수");
+//     break;
+// }
+
+// // for문
+// for (let i = 1; i < 8; i++) {
+//   console.log(i);
+// }
+// for (let i = 0; i < 10; i++) {
+//   if (i === 7) {
+//     // continue;
+//     // break;
+//   }
+//   console.log(i);
+// }
+
+// 함수
+// function bok(main) {
+//   console.log(`${main}볶음밥`);
+// }
+
+// bok("새우");
+// bok("김치");
+// bok("제육");
+
+// const sum = (a, b) => {
+//   return a + b;
+// };
+// console.log(sum(10, 20));
+
+const hello = () => {
+  console.log("Hello");
+  console.log("asd");
+};
+
+const sum = (a, b) => {
+  return a + b;
+};
+console.log(sum(10, 20));
+
+const greeting = (user) => {
+  console.log(`Hello, ${user}`);
+};
+greeting("이윤호");
